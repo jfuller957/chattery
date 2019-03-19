96342d77b6d823ca788f96d8faf1d6ff2df92db1
James Fuller <jfuller957@gmail.com>
2019-03-19 15:49:21 -0700

    socket.io chat room started

---
+++ index.js
@@ -0,0 +1,32 @@
+let app = require("express")();
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg) {
+    console.log("message: " + msg);
+  });
+});
+
+io.on("connection", function(socket) {
+  console.log("a user has connected");
+  io.emit("chat message", "A user has connected!");
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+    io.emit("chat message", "A user has disconnected!");
+  });
+});
+
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
