4ef4b0472a69f3e3598bca8c66e5f69da832c61b
James Fuller <jfuller957@gmail.com>
2019-03-19 21:19:55 -0700

    added username functionality

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-let app = require("express")();
-let http = require("http").Server(app);
-let io = require("socket.io")(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const nicknames = [];
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", function(socket) {
-  socket.on("chat message", function(msg) {
-    console.log("message: " + msg);
+io.sockets.on("connection", function(socket) {
+  socket.on("new user", function(data, callback) {
+    if (nicknames.indexOf(data) != -1) {
+      callback(false);
+    } else {
+      callback(true);
+      socket.nickname = data;
+      nicknames.push(socket.nickname);
+      io.sockets.emit("usernames", nicknames);
+    }
   });
-});
 
-io.on("connection", function(socket) {
-  console.log("a user has connected");
-  io.emit("chat message", "A user has connected!");
   socket.on("disconnect", function() {
-    console.log("user disconnected");
+    io.emit("chat message", "A user has connected!");
+  });
+
+  socket.on("disconnect", function() {
     io.emit("chat message", "A user has disconnected!");
   });
-});
 
-io.on("connection", function(socket) {
   socket.on("chat message", function(msg) {
     io.emit("chat message", msg);
   });
